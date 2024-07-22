2a0ea14e011304690d7d4c742170196fe32b8bbd
Ross Jones <ross@appworks.co.za>
2024-07-22 20:49:16 +0200

    Add script

---
+++ QrCodeScript/QrCodeScript/Program.cs
@@ -0,0 +1,19 @@
+﻿using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+var qrGenerator = new QRCodeGenerator();
+var count = 0;
+
+foreach (var data in File.ReadAllLines("vouchers.csv").Select(d => d.Split(';')).Skip(1))
+{
+    count++;
+
+    var generator = new Url(data[1]);
+    string payload = generator.ToString();
+
+    var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+    var qrCode = new PngByteQRCode(qrCodeData);
+    var graphic = qrCode.GetGraphic(40);
+
+    File.WriteAllBytes($"{count}.png", graphic);
+}
